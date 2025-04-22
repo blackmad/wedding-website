7f9c5df0b8b7ed7d6c648a559d975e613d7ad5a2
David Blackman <david@whizziwig.com>
2025-04-22 23:36:53 +0200

    more places

--- static/js/moda.js
+++ static/js/moda.js
@@ -4,7 +4,7 @@ let data = {
       "latitude": 40.9894843,
       "longitude": 29.024417699999997,
       "name": "Tatar Salim",
-      "description": "I love their doner, Yuva's mother loves their doner, Yuva has grown to love their doner - they have a few outlets around the city",
+      "description": "Yuva, David and Yuva's mother all love this place. Tatar Salim has a few outlets around the city that are all good.",
       "place_id": "ChIJr34jqyTGyhQRFxNDRFDyNA4",
       "google_maps_link": "https://www.google.com/maps/place/?q=place_id:ChIJr34jqyTGyhQRFxNDRFDyNA4&query_place_id=ChIJr34jqyTGyhQRFxNDRFDyNA4",
       "place_name": "Tatar Salim",
@@ -1221,6 +1221,186 @@ let data = {
         "height": 4032,
         "html_attributions": [
           "<a href=\"https://maps.google.com/maps/contrib/117688233977099002305\">OSMAN KENAN</a>"
+        ]
+      },
+      "photo_available": true
+    },
+    {
+      "name": "baris manco house",
+      "description": "Psychedelic rock god of turkey, his home is preserved as a small museum in moda.",
+      "latitude": null,
+      "longitude": null,
+      "place_id": "ChIJc8P60mG4yhQRB2P-O0HJD2w",
+      "google_maps_link": "https://www.google.com/maps/place/?q=place_id:ChIJc8P60mG4yhQRB2P-O0HJD2w",
+      "place_name": "Barış Manço House Museum",
+      "formatted_address": "Caferağa, Yusuf Kamil Paşa Sk. No:5, 34710 Kadıköy/İstanbul, Türkiye",
+      "address_components": [
+        {
+          "long_name": "No:5",
+          "short_name": "No:5",
+          "types": [
+            "street_number"
+          ]
+        },
+        {
+          "long_name": "Yusuf Kamil Paşa Sokak",
+          "short_name": "Yusuf Kamil Paşa Sk.",
+          "types": [
+            "route"
+          ]
+        },
+        {
+          "long_name": "Caferağa",
+          "short_name": "Caferağa",
+          "types": [
+            "administrative_area_level_4",
+            "political"
+          ]
+        },
+        {
+          "long_name": "Kadıköy",
+          "short_name": "Kadıköy",
+          "types": [
+            "administrative_area_level_2",
+            "political"
+          ]
+        },
+        {
+          "long_name": "İstanbul",
+          "short_name": "İstanbul",
+          "types": [
+            "administrative_area_level_1",
+            "political"
+          ]
+        },
+        {
+          "long_name": "Türkiye",
+          "short_name": "TR",
+          "types": [
+            "country",
+            "political"
+          ]
+        },
+        {
+          "long_name": "34710",
+          "short_name": "34710",
+          "types": [
+            "postal_code"
+          ]
+        }
+      ],
+      "website": "http://barismanco.kadikoy.bel.tr/",
+      "phone": "(0216) 337 94 13",
+      "rating": 4.8,
+      "rating_count": 5171,
+      "price_level": null,
+      "categories": [
+        "museum",
+        "point_of_interest",
+        "establishment"
+      ],
+      "open_now": false,
+      "opening_hours": [
+        "Monday: Closed",
+        "Tuesday: 9:00 AM – 4:00 PM",
+        "Wednesday: 9:00 AM – 4:00 PM",
+        "Thursday: 9:00 AM – 4:00 PM",
+        "Friday: 9:00 AM – 4:00 PM",
+        "Saturday: 9:00 AM – 4:00 PM",
+        "Sunday: 9:00 AM – 4:00 PM"
+      ],
+      "photo": {
+        "remote_url": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=AeeoHcLLCHouEpr7ouVD6-Il-b-6pAPajxyQk0dCRwetUm7LBZje2L__iRBeVRF_00PLbmUUvFq3te4Moux7ctKGusTOrmpY6EItnus3aZMzD4ovc0VS1qlKdyJo_nZxMnV0dhkiC-RXtyNQmgnTei6ci45WBGp3x-i7BPA_loxqHlt9okoGwzlKnkw3Y8EAykg7QSYDgDCEVpjklPzYqmdr3C0KtQhj-S7UfEFFVBZlN2T2QjcMNGKzAAQV0vGHhBMFU7QrCHGWiSj2MeMflQoAJt43WUBpk58fADVQHhiLywNgrpc9lw9h229JD9TMtzp0HLTYjG0wiWAPol-EcdbMz5d9l4OZuONv-ar43tqtJzYnxdqLUMPD1m7ykfa7DzuNFm_vmWZV2na3CRmExdrG8syndA270ja2rnfDmX-oiyxb3Xqw04sFIeR6gFM5MjGThFsFiFIaPgOrkiMLN7-hucsYWF7st3jETtvnPT4ze4c_il4ZFNaqIuAbCuaUV4OCpSHtnJrjSk0wc_Hw0206Q463YdbDZvzKxzCMvNk5YyKugYCYRIvbzZTsE5qYGnLDmUbvxL_xogC5jtr9DeZwDuaKLWqYvpFxRokn3IgoZGi6fXGQARaVNpKEb5pPwKfEAJDD0eUq&key=AIzaSyDatAG3RSFl4ULSKseO7ciADYWgYi_1B64",
+        "local_path": "barış_manço_house_museum_7b8389518a48bdfe35b0ad15341264b5.jpg",
+        "width": 4160,
+        "height": 3120,
+        "html_attributions": [
+          "<a href=\"https://maps.google.com/maps/contrib/105381073407507498214\">Emre Coşkun</a>"
+        ]
+      },
+      "photo_available": true
+    },
+    {
+      "name": "rexx cinema",
+      "description": "Great indie / art-house theater. Decent popcorn!",
+      "latitude": null,
+      "longitude": null,
+      "place_id": "ChIJH6z18ma4yhQRiFfVVoz_ZxY",
+      "google_maps_link": "https://www.google.com/maps/place/?q=place_id:ChIJH6z18ma4yhQRiFfVVoz_ZxY",
+      "place_name": "Rexx Cinema",
+      "formatted_address": "Caferağa, Sakız Gülü Sk. No:20, 34710 Kadıköy/İstanbul, Türkiye",
+      "address_components": [
+        {
+          "long_name": "20",
+          "short_name": "20",
+          "types": [
+            "street_number"
+          ]
+        },
+        {
+          "long_name": "Sakız Gülü Sokak",
+          "short_name": "Sakız Gülü Sk.",
+          "types": [
+            "route"
+          ]
+        },
+        {
+          "long_name": "Caferağa",
+          "short_name": "Caferağa",
+          "types": [
+            "administrative_area_level_4",
+            "political"
+          ]
+        },
+        {
+          "long_name": "Kadıköy",
+          "short_name": "Kadıköy",
+          "types": [
+            "administrative_area_level_2",
+            "political"
+          ]
+        },
+        {
+          "long_name": "İstanbul",
+          "short_name": "İstanbul",
+          "types": [
+            "administrative_area_level_1",
+            "political"
+          ]
+        },
+        {
+          "long_name": "Türkiye",
+          "short_name": "TR",
+          "types": [
+            "country",
+            "political"
+          ]
+        },
+        {
+          "long_name": "34710",
+          "short_name": "34710",
+          "types": [
+            "postal_code"
+          ]
+        }
+      ],
+      "website": "http://www.rexxcinema.com/",
+      "phone": "(0216) 337 05 65",
+      "rating": 4.1,
+      "rating_count": 3002,
+      "price_level": null,
+      "categories": [
+        "movie_theater",
+        "point_of_interest",
+        "establishment"
+      ],
+      "photo": {
+        "remote_url": "https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=AeeoHcJrY9Uz_jVPFH0LRObV_5YKamit5xB25967Dv-03S5BIZ8UcfrEXA5aNR2KWgkvIaFz11n_5od_6F0T8_G7I1CAgNKqtUCq59C81S539xmkokwn-tJzoy5GG1n8ICB1ou0fHqxRh36UbJcwJIqCY6OP35aBbGRE8Df_3-6SGj2B-nKFJM33e98pOtqwoo2E9vEpu1gTatoOzYk6X7rPuiTWV4eoMCMGqx_ZXIDg66Viye1ZGuoqGbN5F2HHimNfR9AVl2idPP4nS4TvNDBvwTmSGqFtshMxOkR9ycaBeRb7gqvnnX3N4cdc-IuPyLgPrwYeOKif6xgVZZBm769tPdUmTBxh3RGtU-BraM8fmb6UWlOhU2YjlbkF_hIO_adLQ6geIZE3fI8eS4G3Clrkm8_rKPBtLSK-S43gwD5HlcUWwYcXRzH3cODLaty7-fVHqC81gzXYu0vwhbgI5iVNxQyPmWMBTeKa78x-0k0GP7TOeNbCK--NyVuhasnSdl4_bfnRPbfXGmTXwWHnjSAwLddqFssVd1xqtFxM6PlpBp1nog5-ZX2x2iCAx1tdkiHY94wxtmvfZEeeJ4atwdTvdJK3MNk_SSs1YpY-q3e-L5RwmMqedkYn_edjgzFO4s1F&key=AIzaSyDatAG3RSFl4ULSKseO7ciADYWgYi_1B64",
+        "local_path": "rexx_cinema_907735fa79595e59246273cc82f3d925.jpg",
+        "width": 4032,
+        "height": 3016,
+        "html_attributions": [
+          "<a href=\"https://maps.google.com/maps/contrib/117982511554925599340\">E. Devrim</a>"
         ]
       },
       "photo_available": true
